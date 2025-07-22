a03c7ebed6f172b021d2d75ff09fb38b9b470ad2
GTSBrad <liquid.brad@gmail.com>
2025-07-22 19:43:47 -0400

    added additional books, imported sys and added arguments

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from stats import get_num_words, letters_in_book, get_book_text, sort_chars
+import sys 
 
 def main():
-    book_path = "books/frankenstein.txt" 
+    if len(sys.argv) != 2:
+        print("Usage: python main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
     num_words = get_num_words(book_path)
     characters = letters_in_book(book_path)
     book = get_book_text(book_path) 
     sorted_chars = sort_chars(characters)
-
+    
 
     print("============ BOOKBOT ============")
     print("Analyzing book found at" f" {book_path}")
@@ -17,10 +21,4 @@ def main():
         #return f"{item['char']}: {item['count']}"
         print(f"{item['char']}: {item['count']}")
     print("============= END ===============")
-
-
-
-
-    
- 
 main()
